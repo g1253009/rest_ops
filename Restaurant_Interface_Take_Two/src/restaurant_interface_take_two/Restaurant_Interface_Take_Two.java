281930e3dc0531d948d569e0eca97939218c4463
Tom Ledger <thomas.ledger12@imperial.ac.uk>
2013-02-22 14:20:28 +0000

    Restaurant Interface commit in Netbeans - First revision

---
+++ Restaurant_Interface_Take_Two/src/restaurant_interface_take_two/Restaurant_Interface_Take_Two.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurant_interface_take_two;
+
+/**
+ *
+ * @author thomas_ledger
+ */
+public class Restaurant_Interface_Take_Two {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
